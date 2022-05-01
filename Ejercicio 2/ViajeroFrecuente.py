cdef09f5aa1b7f4699aefc07e5b52c79c1592fd3
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-05-01 10:59:32 -0300

    Add files via upload

---
+++ Ejercicio 2/ViajeroFrecuente.py
@@ -0,0 +1,25 @@
+class ViajeroFrecuente:
+    __num_viajero = 0
+    __dni = ''
+    __nombre = ''
+    __apellido = ''
+    __millas_acum = 0
+    def __init__(self, num_viajero, dni, nombre, apellido, millas_acum):
+        self.__num_viajero = str(num_viajero)
+        self.__dni = int(dni)
+        self.__nombre = str(nombre)
+        self.__apellido = str(apellido)
+        self.__millas_acum = int(millas_acum)
+    def cantidadTotaldeMillas(self):
+        return self.__millas_acum
+    def acumularMillas(self, acumulacionMillas):
+        self.__millas_acum += acumulacionMillas
+        return self.__millas_acum
+    def canjearMillas(self, millasACanjear):
+        if millasACanjear <= self.__millas_acum:
+            self.__millas_acum -= millasACanjear
+            return self.__millas_acum
+        else:
+            print("Error en la operación")
+    def getNumViajero(self):
+        return self.__num_viajero
---
+++ Ejercicio 2/main.py
@@ -0,0 +1,24 @@
+from ManejadorViajeros import ManejadorViajero
+
+if __name__ == '__main__':
+    lista = ManejadorViajero()
+    lista.testViajeros()
+    indice = lista.buscarViajero(input("Ingrese numero de viajero: "))
+    if indice != None:
+        opcion = int(input("MENU:\nOpción 1: Consultar cantidad de Millas\nOpción 2: Acumular Millas\nOpcion 3: Canjear Millas\nOpcion 4: Salir\nEligir opción: "))
+        while opcion != 4:
+            if opcion == 1:
+                print('El Viajero tiene {} millas'.format(lista.getMillasdeViajero(indice)))
+            elif opcion == 2:
+                millasAacumular = int(input("Ingrese cantidad de millas para acumular: "))
+                lista.acumularMillas(indice, millasAacumular)
+                print('El Viajero tiene {} millas'.format(lista.getMillasdeViajero(indice)))
+            elif opcion == 3:
+                millasAcanjear = int(input("Ingrese cantidad de millas a canjear: "))
+                lista.canjearMillas(indice, millasAcanjear)
+                print('El Viajero tiene {} millas'.format(lista.getMillasdeViajero(indice)))
+            else:
+                print("Opción ingresada no corresponde")
+            opcion = int(input("MENU:\nOpción 1: Consultar cantidad de Millas\nOpción 2: Acumular Millas\nOpcion 3: Canjear Millas\nOpcion 4: Salir\nEligir opción: "))
+    else:
+        print("Número de viajero no corresponde")
