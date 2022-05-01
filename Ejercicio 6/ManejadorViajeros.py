a7676018f450d9281faa4c5b66e7d523800847e0
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-05-01 11:24:12 -0300

    Add files via upload

---
+++ Ejercicio 6/ManejadorViajeros.py
@@ -0,0 +1,40 @@
+import csv
+from ViajeroFrecuente import ViajeroFrecuente
+
+class ManejadorViajero:
+    __listaViajeros = None
+    def __init__(self):
+        self.__listaViajeros = []
+    def agregarViajero(self, unViajero):
+        self.__listaViajeros.append(unViajero)
+    def testViajeros(self):
+        archivo = open("archivoViajeros.csv")
+        reader = csv.reader(archivo, delimiter=';')
+        band = True
+        for fila in reader:
+            if (band == True):
+                band = False
+            else:
+                numeroViajero = fila[0]
+                dni = fila[1]
+                nombre = fila[2]
+                apellido = fila[3]
+                millas = fila[4]
+                unViajero = ViajeroFrecuente(numeroViajero, dni, nombre, apellido, millas)
+                self.agregarViajero(unViajero)
+        archivo.close()
+    def mostrarLista(self):
+        for elemento in self.__listaViajeros:
+            print('ID: {} - {} {:10} - Millas: {}'.format(elemento.getNumViajero(), elemento.getNombre(), elemento.getApellido(), elemento.cantidadTotaldeMillas()))
+    def buscarViajero(self, numViajero):
+        for indice, viajero in enumerate(self.__listaViajeros):
+            if viajero.getNumViajero() == numViajero:
+                return indice
+    def getMillasdeViajero(self, numViajero):
+        return self.__listaViajeros[numViajero].cantidadTotaldeMillas()
+    def acumularMillas(self, numViajero, millas):
+        return self.__listaViajeros[numViajero] + millas
+    def canjearMillas(self, numViajero, millas):
+        return self.__listaViajeros[numViajero] - millas
+    def ordenarLista(self):
+        self.__listaViajeros.sort()
