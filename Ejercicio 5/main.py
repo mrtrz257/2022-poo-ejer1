4d59b907a77d5daa8ce2bbc37a42322d9b696d80
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-05-04 19:36:43 -0300

    Update main.py

--- Ejercicio 5/main.py
+++ Ejercicio 5/main.py
@@ -20,3 +20,7 @@ if __name__=='__main__':
         else:
             print("Opción Ingresada no corresponde")
         opcion = int(input("/-------------------/MENU/-------------------/\nOpción 1: Actualizar el Valor del Vehiculo de cada plan\nOpción 2: Mostrar Información segun valor de cuota\nOpción 3: Mostrar Monto para Licitar Vehiculos\nOpcion 4: Modificar cantidad de Cuotas Pagas para Licitar\nOpción 5: Salir\nEligir opción: "))
+    print("TEST:")
+    test1 = PlanAhorro(320, "Ranger", "XLT", 7200000,)
+    print(test1)
+    print('Cuotas: {} - Cantidad de cuotas para licitar: {}'.format(test1.getCantidadCuotas(), test1.getCuotasPagas()))
