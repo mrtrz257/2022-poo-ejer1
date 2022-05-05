8260daaa72cdce5e880d3770734738edb04628c6
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-05-05 19:49:49 -0300

    Update main.py

--- Ejercicio 2/main.py
+++ Ejercicio 2/main.py
@@ -1,7 +1,14 @@
 from ViajeroFrecuente import  ViajeroFrecuente
 from ManejadorViajeros import ManejadorViajero
 
+def test():
+    print("TEST:")      #Crea un objeto de la clase ViajeroFrecuente y la muestra para verificar que se creo correctamente
+    control1 = ViajeroFrecuente(5, 43340662, "Nicolas", "Garcia", 6500.89)
+    print(control1)
+
+
 if __name__ == '__main__':
+    test()
     lista = ManejadorViajero()      #Crea objeto de la clase manejador
     lista.testViajeros()            #Abre el archivo y crea instancias de la clase ViajeroFrecuente para almacenar en la lista
     indice = lista.buscarViajero(input("Ingrese numero de viajero: "))  
@@ -23,6 +30,4 @@ if __name__ == '__main__':
             opcion = int(input("MENU:\nOpción 1: Consultar cantidad de Millas\nOpción 2: Acumular Millas\nOpcion 3: Canjear Millas\nOpcion 4: Salir\nEligir opción: "))
     else:
         print("Número de viajero no corresponde")
-    print("TEST:")      #Crea un objeto de la clase ViajeroFrecuente y la muestra para verificar que se creo correctamente
-    control1 = ViajeroFrecuente(5, 43340662, "Nicolas", "Garcia", 6500.89)
-    print(control1)
+    
