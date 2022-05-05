e128a4fe132e178589a3e2e0443e57915b35a073
mrtrz257 <83235511+mrtrz257@users.noreply.github.com>
2022-05-05 19:51:36 -0300

    Update main.py

--- Ejercicio 5/main.py
+++ Ejercicio 5/main.py
@@ -1,7 +1,15 @@
 from ManejadorPlanes import Manejador
 from PlanAhorro import PlanAhorro
 
+def test():
+    print("TEST:")      #Crea un objeto de la clase PlanAhorro y la muestra para verificar que se creo correctamente
+    test1 = PlanAhorro(320, "Ranger", "XLT", 7200000,)
+    print(test1)
+    print('Cuotas: {} - Cantidad de cuotas para licitar: {}'.format(test1.getCantidadCuotas(), test1.getCuotasPagas()))
+
+
 if __name__=='__main__':
+    test()
     lista = Manejador()
     lista.testPlanes()
     opcion = int(input("/-------------------/MENU/-------------------/\nOpción 1: Actualizar el Valor del Vehiculo de cada plan\nOpción 2: Mostrar Información segun valor de cuota\nOpción 3: Mostrar Monto para Licitar Vehiculos\nOpcion 4: Modificar cantidad de Cuotas Pagas para Licitar\nOpción 5: Salir\nEligir opción: "))
@@ -21,7 +29,4 @@ if __name__=='__main__':
         else:
             print("Opción Ingresada no corresponde")
         opcion = int(input("/-------------------/MENU/-------------------/\nOpción 1: Actualizar el Valor del Vehiculo de cada plan\nOpción 2: Mostrar Información segun valor de cuota\nOpción 3: Mostrar Monto para Licitar Vehiculos\nOpcion 4: Modificar cantidad de Cuotas Pagas para Licitar\nOpción 5: Salir\nEligir opción: "))
-    print("TEST:")      #Crea un objeto de la clase PlanAhorro y la muestra para verificar que se creo correctamente
-    test1 = PlanAhorro(320, "Ranger", "XLT", 7200000,)
-    print(test1)
-    print('Cuotas: {} - Cantidad de cuotas para licitar: {}'.format(test1.getCantidadCuotas(), test1.getCuotasPagas()))
+    
